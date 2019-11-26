4c95a7ec4ee93d2a5dfd801ce471234b96eb5a55
JSBastos <jailsonsousa11@gmail.com>
2019-11-26 05:21:47 -0300

    Update database.js

--- GoNode/module_2/gobarber/src/config/database.js
+++ GoNode/module_2/gobarber/src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
-  username: 'postgres',
-  password: 'asd123asd',
+  username: '',
+  password: '',
   database: 'gobarber',
   operatorAliases: false,
   define: {
